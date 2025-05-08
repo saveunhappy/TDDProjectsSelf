538ce7d59cf5fce4ee707fe18381f061dbed7f35
houjiangtao <925147954@qq.com>
2025-05-08 15:15:43 +0800

    1.这个是多了两个参数，直接就是通过的，说明我们的这个测试是多余的，之前的测试已经直接给覆盖到了

--- args_02/src/test/java/com/geektime/tdd/args/BooleanOptionParserTest.java
+++ args_02/src/test/java/com/geektime/tdd/args/BooleanOptionParserTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -13,8 +14,16 @@ public class BooleanOptionParserTest {
     public void should_not_accept_extra_argument_for_boolean_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class,
                 () -> new BooleanOptionParser().parse(Arrays.asList("-l", "t"), option("l")));
-        assertEquals("l",e.getOption());
+        assertEquals("l", e.getOption());
     }
+
+    @Test
+    public void should_not_accept_extra_arguments_for_boolean_option() {
+        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class,
+                () -> new BooleanOptionParser().parse(asList("-l", "t", "f"), option("l")));
+        assertEquals("l", e.getOption());
+    }
+
 
     static Option option(String value) {
         return new Option() {
