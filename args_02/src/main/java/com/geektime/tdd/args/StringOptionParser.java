bef7851afb629f58535bf9a9e66debea61ade271
houjiangtao <925147954@qq.com>
2025-05-08 14:28:08 +0800

    1.就这么用，方法也可以去掉了

--- args_02/src/main/java/com/geektime/tdd/args/StringOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/StringOptionParser.java
@@ -1,11 +1,8 @@
 package com.geektime.tdd.args;
 
-import java.util.List;
-
 class StringOptionParser extends IntOptionParser {
 
-
-    protected Object parseValue(String value) {
-        return String.valueOf(value);
+    public StringOptionParser() {
+        super(String::valueOf);
     }
 }
