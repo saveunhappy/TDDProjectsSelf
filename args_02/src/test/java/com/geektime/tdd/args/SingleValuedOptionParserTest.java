663d4ecb40c41a6e9358bb028ca6225e79301f8e
houjiangtao <925147954@qq.com>
2025-05-08 16:34:25 +0800

    1.现在String和int是一个,所以测试是直接通过的

--- args_02/src/test/java/com/geektime/tdd/args/SingleValuedOptionParserTest.java
+++ args_02/src/test/java/com/geektime/tdd/args/SingleValuedOptionParserTest.java
@@ -29,7 +29,15 @@ public class SingleValuedOptionParserTest {
 
     @Test
     public void should_set_default_value_to_0_for_int_option() throws Exception {
-        assertEquals(0, new SingleValueOptionParser<Integer>(0, Integer::parseInt).parse(asList(), option("-p")));
+        assertEquals(0, new SingleValueOptionParser<>(0, Integer::parseInt).parse(asList(), option("-p")));
+    }
+
+    @Test
+    public void should_not_accept_extra_argument_for_String_single_valued_option() throws Exception {
+        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () ->
+                new SingleValueOptionParser<>("", String::valueOf)
+                        .parse(asList("-d", "/usr/logs", "/usr/vars"), option("d")));
+        assertEquals("d", e.getOption());
     }
 
 }
