7c812c704fde22a73e92ea7ef3eba6457b69472d
houjiangtao <925147954@qq.com>
2025-05-08 14:10:22 +0800

    1.看String,如果inline进去进行抽取，那么会把逻辑给带进去，就不是我们想要的了

--- args_02/src/main/java/com/geektime/tdd/args/IntOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/IntOptionParser.java
@@ -8,6 +8,10 @@ class IntOptionParser implements OptionParser {
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         String value = arguments.get(index + 1);
+        return parseValue(value);
+    }
+
+    private static int parseValue(String value) {
         return Integer.parseInt(value);
     }
 }
--- args_02/src/main/java/com/geektime/tdd/args/StringOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/StringOptionParser.java
@@ -7,7 +7,10 @@ class StringOptionParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-        String value = arguments.get(index + 1);
-        return String.valueOf(value);
+        return getString(arguments, index);
+    }
+
+    private static String getString(List<String> arguments, int index) {
+        return String.valueOf(arguments.get(index + 1));
     }
 }
