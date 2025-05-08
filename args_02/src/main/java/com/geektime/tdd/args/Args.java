707982e2169a87598c7fb847543072aa29da65b1
houjiangtao <925147954@qq.com>
2025-05-08 09:28:08 +0800

    1.先让他通过，但是这个是最假的，连参数都没有用到，但是没关系，一步一步来

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -1,9 +1,16 @@
 package com.geektime.tdd.args;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Args {
 
-    public static <T> T parse(Class<T> optionsClass, String ...args) {
-
-        return null;
+    public static <T> T parse(Class<T> optionsClass, String ... args) {
+        Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+        try {
+            return (T) constructor.newInstance(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
