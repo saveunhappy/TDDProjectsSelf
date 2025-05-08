d92d7f426cc5ba2a4f76f20fedfee54d4e545b0a
houjiangtao <925147954@qq.com>
2025-05-08 09:51:33 +0800

    1.调整一下try的位置

--- args_02/src/main/java/com/geektime/tdd/args/Args.java
+++ args_02/src/main/java/com/geektime/tdd/args/Args.java
@@ -8,24 +8,24 @@ import java.util.List;
 
 public class Args {
 
-    public static <T> T parse(Class<T> optionsClass, String ... args) {
-        Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-        Parameter parameter = constructor.getParameters()[0];
-        Option option = parameter.getAnnotation(Option.class);
-        List<String> arguments = Arrays.asList(args);
-        Object value = null;
-        if(parameter.getType() == boolean.class){
-            value = arguments.contains("-" + option.value());
-        }
-        if(parameter.getType() == int.class){
-            int index = arguments.indexOf("-" + option.value());
-            value = Integer.parseInt(arguments.get(index + 1));
-        }
-        if(parameter.getType() == String.class){
-            int index = arguments.indexOf("-" + option.value());
-            value = arguments.get(index + 1);
-        }
+    public static <T> T parse(Class<T> optionsClass, String... args) {
         try {
+            List<String> arguments = Arrays.asList(args);
+            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+            Parameter parameter = constructor.getParameters()[0];
+            Option option = parameter.getAnnotation(Option.class);
+            Object value = null;
+            if (parameter.getType() == boolean.class) {
+                value = arguments.contains("-" + option.value());
+            }
+            if (parameter.getType() == int.class) {
+                int index = arguments.indexOf("-" + option.value());
+                value = Integer.parseInt(arguments.get(index + 1));
+            }
+            if (parameter.getType() == String.class) {
+                int index = arguments.indexOf("-" + option.value());
+                value = arguments.get(index + 1);
+            }
             return (T) constructor.newInstance(value);
         } catch (Exception e) {
             throw new RuntimeException(e);
