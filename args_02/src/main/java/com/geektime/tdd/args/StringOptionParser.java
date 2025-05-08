c923019758219939294091870e4237ee34409e96
houjiangtao <925147954@qq.com>
2025-05-08 11:00:04 +0800

    1.现在看着Int的和String的很像，只是最后返回值那边调用的方法不一样，那么我们先在String写一个废话似的，因为本来就是String的，没必要调用String.valueOf

--- args_02/src/main/java/com/geektime/tdd/args/StringOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/StringOptionParser.java
@@ -7,6 +7,6 @@ class StringOptionParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-        return arguments.get(index + 1);
+        return String.valueOf(arguments.get(index + 1));
     }
 }
