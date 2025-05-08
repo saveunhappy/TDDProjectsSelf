4e4319d4def2531c40c3f4b5784641be09671cb4
houjiangtao <925147954@qq.com>
2025-05-08 15:47:37 +0800

    1.这个通过了，但是如果不是-p -l呢？如果是只有一个-p呢？

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -13,6 +13,7 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
+        if(arguments.get(index + 1).startsWith("-")) throw new InsufficientException(option.value());
         if ((index + 2) < arguments.size() && !arguments.get(index + 2).startsWith("-"))
             throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
