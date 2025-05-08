ccc54e3e750b5d07e596212b3c2fcf7d09004453
houjiangtao <925147954@qq.com>
2025-05-08 15:14:31 +0800

    1.单个的OptionParser通过了，但是整体的测试失败了，因为如果你就只传了一个-l呢？那index+1去get那不就索引越界了么，所以要加一个限制，index + 1不能超过参数的个数

--- args_02/src/main/java/com/geektime/tdd/args/BooleanOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/BooleanOptionParser.java
@@ -7,7 +7,8 @@ class BooleanOptionParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-        if(!arguments.get(index + 1).startsWith("-")) throw new TooManyArgumentsException(option.value());
+        if ((index + 1) < arguments.size() && !arguments.get(index + 1).startsWith("-"))
+            throw new TooManyArgumentsException(option.value());
         return index != -1;
     }
 }
