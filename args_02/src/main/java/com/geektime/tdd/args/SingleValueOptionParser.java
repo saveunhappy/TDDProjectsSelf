37787cca34f4ca00b363cf041bba166e6d62dd1a
houjiangtao <925147954@qq.com>
2025-05-08 15:42:08 +0800

    1.这个SingleValue和Boolean那个类似，都是判断那个位置是不是以-开头，并且不能越界

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -13,8 +13,9 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-        String value = arguments.get(index + 1);
-        return valueParser.apply(value);
+        if ((index + 2) < arguments.size() && !arguments.get(index + 2).startsWith("-"))
+            throw new TooManyArgumentsException(option.value());
+        return valueParser.apply(arguments.get(index + 1));
     }
 
 }
