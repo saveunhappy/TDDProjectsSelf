fbc4ad5061b51494d35a1119d7f4092e2131066a
houjiangtao <925147954@qq.com>
2025-05-09 10:17:35 +0800

    1.抽取方法

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -19,15 +19,20 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
+        List<String> values = values(arguments, index);
+
+        if (values.size() < 1) throw new InsufficientException(option.value());
+        if (values.size() > 1) throw new TooManyArgumentsException(option.value());
+        return valueParser.apply(arguments.get(index + 1));
+    }
+
+    private static List<String> values(List<String> arguments, int index) {
         int followingFlag = IntStream.range(index + 1, arguments.size())
                 .filter(it -> arguments.get(it).startsWith("-"))
                 .findFirst()
                 .orElse(arguments.size());
         List<String> values = arguments.subList(index + 1, followingFlag);
-
-        if (values.size() < 1) throw new InsufficientException(option.value());
-        if (values.size() > 1) throw new TooManyArgumentsException(option.value());
-        return valueParser.apply(arguments.get(index + 1));
+        return values;
     }
 
 
