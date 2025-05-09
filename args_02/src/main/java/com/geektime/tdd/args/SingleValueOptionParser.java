8cc29b1c02f4dca9dc0ac0130ec4cdc8d3783e88
houjiangtao <925147954@qq.com>
2025-05-09 10:04:54 +0800

    1.重构这本书中推荐这么做，望文生义，但是其实可以有更好的代码写法，因为这样只是说明了这个方法在做什么，我们可以把代码写的更好一点，不需要这么写解释

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -18,11 +18,23 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (index + 1 == arguments.size() || arguments.get(index + 1).startsWith("-"))
+        if (isReachEndOfList(arguments, index) || isFollowedByOtherFlag(arguments, index))
             throw new InsufficientException(option.value());
-        if ((index + 2) < arguments.size() && !arguments.get(index + 2).startsWith("-"))
+        if (secondArgumentIsNotAFlag(arguments, index))
             throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
+    }
+
+    private static boolean secondArgumentIsNotAFlag(List<String> arguments, int index) {
+        return (index + 2) < arguments.size() && !arguments.get(index + 2).startsWith("-");
+    }
+
+    private static boolean isFollowedByOtherFlag(List<String> arguments, int index) {
+        return arguments.get(index + 1).startsWith("-");
+    }
+
+    private static boolean isReachEndOfList(List<String> arguments, int index) {
+        return index + 1 == arguments.size();
     }
 
 }
