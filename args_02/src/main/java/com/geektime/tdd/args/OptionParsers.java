3b6a968af0ca08241799a143b0398a31ff2dc3dc
houjiangtao <925147954@qq.com>
2025-05-09 16:41:05 +0800

    1.inline之前的，之前忘了

--- args_02/src/main/java/com/geektime/tdd/args/OptionParsers.java
+++ args_02/src/main/java/com/geektime/tdd/args/OptionParsers.java
@@ -3,6 +3,7 @@ package com.geektime.tdd.args;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.IntStream;
 
 class OptionParsers<T> implements OptionParser<T> {
@@ -22,20 +23,21 @@ class OptionParsers<T> implements OptionParser<T> {
     }
 
     public static <T> OptionParser<T> unary(T defaultValue, Function<String, T> valueParser) {
-        return (arguments, option) -> getT(arguments, option, valueParser, defaultValue);
+        return (arguments, option) -> values(arguments, option, 1)
+                .map(it -> parseValue(it.get(0), valueParser))
+                .orElse(defaultValue);
+    }
+    public static <T> OptionParser<T[]> list(IntFunction<T[]> generator, Function<String, T> valueParser) {
+        return null;
     }
 
     @Override
     public T parse(List<String> arguments, Option option) {
 
-        return getT(arguments, option, valueParser, defaultValue);
-
-    }
-
-    private static <T> T getT(List<String> arguments, Option option, Function<String, T> valueParser, T defaultValue) {
         return values(arguments, option, 1)
                 .map(it -> parseValue(it.get(0), valueParser))
                 .orElse(defaultValue);
+
     }
 
     static Optional<List<String>> values(List<String> arguments, Option option, int expectedSize) {
