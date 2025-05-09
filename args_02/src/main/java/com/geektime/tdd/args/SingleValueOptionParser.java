1d1bcbf500fde8b660e32cb3449b8af1ee7c59bf
houjiangtao <925147954@qq.com>
2025-05-09 10:15:24 +0800

    1.-p 8080 8090 -d /usr/logs这个方法就是找到-d的位置，然后sublist就是取得-p和-d之间，也就是8080和8090切割出来的list，那么直接判断这个list的长度就好了

--- args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
+++ args_02/src/main/java/com/geektime/tdd/args/SingleValueOptionParser.java
@@ -2,6 +2,7 @@ package com.geektime.tdd.args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingleValueOptionParser<T> implements OptionParser<T> {
 
@@ -18,23 +19,16 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (isReachEndOfList(arguments, index) || isFollowedByOtherFlag(arguments, index))
-            throw new InsufficientException(option.value());
-        if (secondArgumentIsNotAFlag(arguments, index))
-            throw new TooManyArgumentsException(option.value());
+        int followingFlag = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst()
+                .orElse(arguments.size());
+        List<String> values = arguments.subList(index + 1, followingFlag);
+
+        if (values.size() < 1) throw new InsufficientException(option.value());
+        if (values.size() > 1) throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
     }
 
-    private static boolean secondArgumentIsNotAFlag(List<String> arguments, int index) {
-        return (index + 2) < arguments.size() && !arguments.get(index + 2).startsWith("-");
-    }
-
-    private static boolean isFollowedByOtherFlag(List<String> arguments, int index) {
-        return arguments.get(index + 1).startsWith("-");
-    }
-
-    private static boolean isReachEndOfList(List<String> arguments, int index) {
-        return index + 1 == arguments.size();
-    }
 
 }
