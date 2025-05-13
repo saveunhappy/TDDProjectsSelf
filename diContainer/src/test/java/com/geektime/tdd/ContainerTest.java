3c02a6bbe1fcbef8dd6e1a605c5af2a1de15c744
houjiangtao <925147954@qq.com>
2025-05-13 10:25:25 +0800

    1.三大块内容，依赖注入，依赖选择，生命周期管理

---
+++ diContainer/src/test/java/com/geektime/tdd/ContainerTest.java
@@ -0,0 +1,21 @@
+package com.geektime.tdd;
+
+import org.junit.jupiter.api.Nested;
+
+class ContainerTest {
+
+    @Nested
+    public class ComponentConstruction{
+
+    }
+
+    @Nested
+    public class DependenciesSelection{
+
+    }
+
+    @Nested
+    public class LifecycleManagement{
+
+    }
+}
