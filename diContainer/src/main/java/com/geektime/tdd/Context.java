19daa2ea48f7402ead8eccea1f1f593f9dca3f46
houjiangtao <925147954@qq.com>
2025-05-13 10:55:17 +0800

    1.这个是instance,是最简单的，就是放到map中再拿出来，没有用到反射

--- diContainer/src/main/java/com/geektime/tdd/Context.java
+++ diContainer/src/main/java/com/geektime/tdd/Context.java
@@ -1,11 +1,16 @@
 package com.geektime.tdd;
 
-public class Context {
-    public <ComponentType> void bind(Class<ComponentType> componentClass, ComponentType component) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Context {
+    private Map<Class<?>,Object> components = new HashMap<>();
+    public <ComponentType> void bind(Class<ComponentType> componentClass, ComponentType component) {
+        components.put(componentClass,component);
 
     }
     public <ComponentType> ComponentType get(Class<ComponentType> componentClass) {
-        return null;
+
+        return (ComponentType) components.get(componentClass);
     }
 }
