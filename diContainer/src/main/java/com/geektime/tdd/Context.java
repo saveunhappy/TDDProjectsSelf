aed8e4807fc2b32209d689724fe613df3cfea01d
houjiangtao <925147954@qq.com>
2025-05-15 10:04:17 +0800

    1.inline回来

--- diContainer/src/main/java/com/geektime/tdd/Context.java
+++ diContainer/src/main/java/com/geektime/tdd/Context.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-    private Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
+    private final Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
 
-    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
+    private final Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
     public <ComponentType> void bind(Class<ComponentType> componentClass, ComponentType component) {
         providers.put(componentClass, () -> component);
@@ -17,7 +17,13 @@ public class Context {
     public <ComponentType, ComponentImplementation extends ComponentType>
     void bind(Class<ComponentType> componentClass, Class<ComponentImplementation> implementation) {
         componentImplementations.put(componentClass, implementation);
-        providers.put(componentClass, () -> getComponentType(implementation));
+        providers.put(componentClass, () -> {
+            try {
+                return implementation.getDeclaredConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public <ComponentType> ComponentType get(Class<ComponentType> componentClass) {
@@ -25,14 +31,11 @@ public class Context {
             return (ComponentType) providers.get(componentClass).get();
         }
         Class<?> implementation = componentImplementations.get(componentClass);
-        return getComponentType(implementation);
-    }
-
-    private <ComponentType> ComponentType getComponentType(Class<?> implementation) {
         try {
             return (ComponentType) implementation.getDeclaredConstructor().newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
 }
