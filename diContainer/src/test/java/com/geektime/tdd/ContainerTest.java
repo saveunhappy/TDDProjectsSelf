522c6d221c816231bd40febdf6fd80021fb47cba
houjiangtao <925147954@qq.com>
2025-05-15 10:35:10 +0800

    1.增加构造函数注入的测试

--- diContainer/src/test/java/com/geektime/tdd/ContainerTest.java
+++ diContainer/src/test/java/com/geektime/tdd/ContainerTest.java
@@ -1,5 +1,6 @@
 package com.geektime.tdd;
 
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,9 +11,10 @@ class ContainerTest {
     Context context;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         context = new Context();
     }
+
     @Nested
     public class ComponentConstruction {
 
@@ -39,6 +41,18 @@ class ContainerTest {
                 assertTrue(component instanceof ComponentWithDefaultConstructor);
             }
             //TODO: with dependencies
+            @Test
+            public void should_bind_type_to_a_class_with_injection_constructor() {
+                Dependency dependency = new Dependency() {
+                };
+                context.bind(Component.class, ComponentWithInjectionDependency.class);
+                context.bind(Dependency.class, dependency);
+                Component component = context.get(Component.class);
+                assertNotNull(component);
+                assertTrue(component instanceof ComponentWithInjectionDependency);
+                assertEquals(dependency, ((ComponentWithInjectionDependency) component).dependency);
+
+            }
             //TODO: A->B->C
         }
 
@@ -67,10 +81,25 @@ class ContainerTest {
     interface Component {
 
     }
+    interface Dependency {
+
+    }
 
     static class ComponentWithDefaultConstructor implements Component {
         public ComponentWithDefaultConstructor() {
         }
     }
+
+
+
+    static class ComponentWithInjectionDependency implements Component {
+        Dependency dependency;
+
+        @Inject
+        public ComponentWithInjectionDependency(Dependency dependency) {
+            this.dependency = dependency;
+        }
+    }
+
 }
 
