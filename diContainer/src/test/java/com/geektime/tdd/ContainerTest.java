048b8247bee35cb7763e82fb63355927c60a46fe
houjiangtao <925147954@qq.com>
2025-05-16 09:51:15 +0800

    1.如果在finally中不设置constructing = false进行重制的话，那么如果调用get方法获取组件只有第一次成功，第二次就会失败，因为第一次没有重制状态，导致后面的constructing一直是true，就他一直认为是循环依赖了，正常循环依赖的场景碰到了也就抛出异常了，程序就终止了，不用考虑设置复位了

--- diContainer/src/test/java/com/geektime/tdd/ContainerTest.java
+++ diContainer/src/test/java/com/geektime/tdd/ContainerTest.java
@@ -99,12 +99,17 @@ class ContainerTest {
 
             @Test
             public void should_throw_exception_if_transitive_cyclic_dependencies_found() throws Exception {
-                context.bind(Component.class,ComponentWithInjectionConstructor.class);
-                context.bind(Dependency.class,DependencyDependedOnAnotherDependency.class);
+                context.bind(Component.class, ComponentWithInjectionConstructor.class);
+                context.bind(Dependency.class, DependencyDependedOnAnotherDependency.class);
                 context.bind(AnotherDependency.class, AnotherDependencyDependedOnComponent.class);
                 assertThrows(CyclicDependenciesFoundException.class, () -> context.get(Component.class));
+            }
 
-
+            @Test
+            public void should_throw_exception_if_constructing_not_reset() {
+                context.bind(Component.class, ComponentWithDefaultConstructor.class);
+                assertDoesNotThrow(() -> context.get(Component.class));
+                assertThrows(CyclicDependenciesFoundException.class, () -> context.get(Component.class));
             }
         }
 
@@ -198,19 +203,23 @@ class ContainerTest {
 
         }
     }
-    static class DependencyDependedOnAnotherDependency implements Dependency{
+
+    static class DependencyDependedOnAnotherDependency implements Dependency {
         AnotherDependency anotherDependency;
+
         @Inject
         public DependencyDependedOnAnotherDependency(AnotherDependency anotherDependency) {
             this.anotherDependency = anotherDependency;
         }
     }
 
-    interface AnotherDependency{
+    interface AnotherDependency {
 
     }
-    static class AnotherDependencyDependedOnComponent implements AnotherDependency{
+
+    static class AnotherDependencyDependedOnComponent implements AnotherDependency {
         Component component;
+
         @Inject
 
         public AnotherDependencyDependedOnComponent(Component component) {
