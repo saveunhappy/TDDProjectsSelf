d009e7df36577f8e5c739e31818ab6fe527cb600
houjiangtao <925147954@qq.com>
2025-05-19 14:06:37 +0800

    1.放到方法里面去

--- diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
+++ diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
@@ -91,7 +91,7 @@ public class ContextConfig {
 
         @Override
         public List<Class<?>> getDependency() {
-            return null;
+            return getCollect(injectConstructor);
         }
     }
 
