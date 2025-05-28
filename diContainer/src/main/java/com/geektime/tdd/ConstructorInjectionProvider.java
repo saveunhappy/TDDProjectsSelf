2fd036775c6261f96461a69c5f88094567d23e72
houjiangtao <925147954@qq.com>
2025-05-28 09:10:42 +0800

    1.Field注入通过

--- diContainer/src/main/java/com/geektime/tdd/ConstructorInjectionProvider.java
+++ diContainer/src/main/java/com/geektime/tdd/ConstructorInjectionProvider.java
@@ -3,6 +3,7 @@ package com.geektime.tdd;
 import jakarta.inject.Inject;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
@@ -13,9 +14,11 @@ import static java.util.Arrays.stream;
 
 class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
     private final Constructor<T> injectConstructor;
+    private List<Field> injectFields;
 
     public ConstructorInjectionProvider(Class<T> injectConstructor) {
         this.injectConstructor = getInjectConstructor(injectConstructor);
+        this.injectFields = getInjectFields(injectConstructor);
     }
 
     private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
@@ -32,6 +35,10 @@ class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
         });
     }
 
+    private static <T> List<Field> getInjectFields(Class<T> component) {
+        return stream(component.getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(Inject.class)).toList();
+    }
 
     @Override
     public T get(Context context) {
@@ -39,7 +46,15 @@ class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
             Object[] array = Arrays.stream(injectConstructor.getParameters())
                     .map(p -> context.get(p.getType()).get())
                     .toArray();
-            return injectConstructor.newInstance(array);
+            T instance = injectConstructor.newInstance(array);
+            for (Field field : injectFields) {
+                //这里直接调用.get()就可以，因为前面的getContext中得到Dependency
+                //之后就会去校验，如果不存在就会抛出异常，所以这里就可以直接调用.get()
+                //现在还没有加上Field的dependency，所以这里的getDependency()是没有用的
+                //但是如果加上Field的dependency，就可以在这里校验了
+                field.set(instance,context.get(field.getType()).get());
+            }
+            return instance;
         } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
