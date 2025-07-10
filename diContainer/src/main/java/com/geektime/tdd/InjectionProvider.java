45560860a59009dc68925509dbe8dd37af57d4bd
houjiangtao <925147954@qq.com>
2025-07-10 10:28:41 +0800

    1.把要合并的那部分都抽取出来，名字取的都是一样的，找公共点，发现都是要传current的，就是当前的这个类，因为要一直往上找

--- diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
+++ diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
@@ -29,50 +29,6 @@ class InjectionProvider<T> implements ComponentProvider<T> {
 
     }
 
-    private static <T> List<Method> getInjectMethods(Class<T> component) {
-
-        List<Method> injectMethods = new ArrayList<>();
-        Class<?> current = component;
-        while (current != Object.class) {
-            injectMethods.addAll(injectable(current.getDeclaredMethods())
-                    //由子到父的添加，子类先添加完，就到injectMethods中了，然后到父类再找到
-                    //去对比子类中是否有同名方法，并且参数个数也相同，因为存在重载，这里就是都被@Inject标注了
-                    //但是只能调用一次，所以就把父类的方法过滤掉了，因为子类中已经有了
-                    .filter(m -> isOverrideByInjectMethod(injectMethods, m))
-                    //第一轮子类的方法没有被@Inject标注，所以是空，第二轮发现父类被标注了@Inject，
-                    // 所以上一步筛选出来了被父类标注的
-                    //然后再去对比，和子类的那个没有被@Inject标注的那个方法名字一样不，一样的话，
-                    //那就得把父类的那个方法去掉了，否则我只new了一个子类，没有@Inject父类同名的方法
-                    //只是起了个同样的名字，那么父类的那个就不应该被调用
-                    .filter(m -> isOverrideByNoInjectMethod(component, m))
-
-                    .toList());
-            current = current.getSuperclass();
-        }
-        Collections.reverse(injectMethods);
-        return injectMethods;
-    }
-
-    private static boolean isOverrideByInjectMethod(List<Method> injectMethods, Method m) {
-        return injectMethods.stream().noneMatch(o -> isOverride(m, o));
-    }
-
-    private static <T> boolean isOverrideByNoInjectMethod(Class<T> component, Method m) {
-        return stream(component.getDeclaredMethods()).filter(m1 -> !m1.isAnnotationPresent(Inject.class))
-                .noneMatch(o -> isOverride(m, o));
-    }
-
-    private static boolean isOverride(Method m, Method o) {
-        return o.getName().equals(m.getName())
-                && Arrays.equals(o.getParameterTypes(), m.getParameterTypes());
-    }
-
-    private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
-        List<Constructor<?>> injectConstructors = injectable(implementation.getConstructors()).toList();
-        if (injectConstructors.size() > 1) throw new IllegalComponentException();
-
-        return (Constructor<Type>) injectConstructors.stream().findFirst().orElseGet(() -> defaultConstructor(implementation));
-    }
 
     private static <Type> Constructor<Type> defaultConstructor(Class<Type> implementation) {
         try {
@@ -81,16 +37,46 @@ class InjectionProvider<T> implements ComponentProvider<T> {
             throw new IllegalComponentException();
         }
     }
+    private static <T> List<Method> getInjectMethods(Class<T> component) {
 
+        List<Method> injectMethods = new ArrayList<>();
+        Class<?> current = component;
+        while (current != Object.class) {
+            injectMethods.addAll(getC(component, injectMethods, current));
+            current = current.getSuperclass();
+        }
+        Collections.reverse(injectMethods);
+        return injectMethods;
+    }
+
+    private static <T> List<Method> getC(Class<T> component, List<Method> injectMethods, Class<?> current) {
+        return injectable(current.getDeclaredMethods())
+                //由子到父的添加，子类先添加完，就到injectMethods中了，然后到父类再找到
+                //去对比子类中是否有同名方法，并且参数个数也相同，因为存在重载，这里就是都被@Inject标注了
+                //但是只能调用一次，所以就把父类的方法过滤掉了，因为子类中已经有了
+                .filter(m -> isOverrideByInjectMethod(injectMethods, m))
+                //第一轮子类的方法没有被@Inject标注，所以是空，第二轮发现父类被标注了@Inject，
+                // 所以上一步筛选出来了被父类标注的
+                //然后再去对比，和子类的那个没有被@Inject标注的那个方法名字一样不，一样的话，
+                //那就得把父类的那个方法去掉了，否则我只new了一个子类，没有@Inject父类同名的方法
+                //只是起了个同样的名字，那么父类的那个就不应该被调用
+                .filter(m -> isOverrideByNoInjectMethod(component, m))
+
+                .toList();
+    }
     private static <T> List<Field> getInjectFields(Class<T> component) {
         List<Field> injectFields = new ArrayList<>();
         Class<?> current = component;
         while (current != Object.class) {
             //注意，这里是current
-            injectFields.addAll(injectable(current.getDeclaredFields()).toList());
+            injectFields.addAll(getC(current));
             current = current.getSuperclass();
         }
         return injectFields;
+    }
+
+    private static List<Field> getC(Class<?> current) {
+        return injectable(current.getDeclaredFields()).toList();
     }
 
     private static <T extends AnnotatedElement> Stream<T> injectable(T[] declaredFields) {
@@ -130,6 +116,28 @@ class InjectionProvider<T> implements ComponentProvider<T> {
                 .toArray();
     }
 
+
+
+    private static boolean isOverrideByInjectMethod(List<Method> injectMethods, Method m) {
+        return injectMethods.stream().noneMatch(o -> isOverride(m, o));
+    }
+
+    private static <T> boolean isOverrideByNoInjectMethod(Class<T> component, Method m) {
+        return stream(component.getDeclaredMethods()).filter(m1 -> !m1.isAnnotationPresent(Inject.class))
+                .noneMatch(o -> isOverride(m, o));
+    }
+
+    private static boolean isOverride(Method m, Method o) {
+        return o.getName().equals(m.getName())
+                && Arrays.equals(o.getParameterTypes(), m.getParameterTypes());
+    }
+
+    private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
+        List<Constructor<?>> injectConstructors = injectable(implementation.getConstructors()).toList();
+        if (injectConstructors.size() > 1) throw new IllegalComponentException();
+
+        return (Constructor<Type>) injectConstructors.stream().findFirst().orElseGet(() -> defaultConstructor(implementation));
+    }
     @Override
     public List<Class<?>> getDependency() {
         return concat(concat(stream(injectConstructor.getParameters()).map(Parameter::getType),
