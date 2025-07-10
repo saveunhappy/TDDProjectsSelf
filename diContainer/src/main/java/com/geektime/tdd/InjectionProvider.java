b7b38948ab79bf8b37c28485ffac9ed7dcadb35e
houjiangtao <925147954@qq.com>
2025-07-10 10:54:23 +0800

    1.先改个名字，编译通过，如果不这样，直接调用之前抽取出来的Field的那个方法是不行的，因为返回的参数一个是Field,一个是Method,改成Executable？试过了，要一个一个改，还不一定对，先改名吧

--- diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
+++ diContainer/src/main/java/com/geektime/tdd/InjectionProvider.java
@@ -42,12 +42,12 @@ class InjectionProvider<T> implements ComponentProvider<T> {
     private static <T> List<Method> getInjectMethods(Class<T> component) {
         BiFunction<List<Method>, Class<?>, List<Method>> function = (methods, current) -> getC(component, methods, current);
 
-        List<Method> injectMethods = traverse(component, function);
+        List<Method> injectMethods = traverse1(component, function);
         Collections.reverse(injectMethods);
         return injectMethods;
     }
 
-    private static <T> List<Method> traverse(Class<T> component, BiFunction<List<Method>, Class<?>, List<Method>> function) {
+    private static <T> List<Method> traverse1(Class<T> component, BiFunction<List<Method>, Class<?>, List<Method>> function) {
         List<Method> injectMethods = new ArrayList<>();
         Class<?> current = component;
         while (current != Object.class) {
