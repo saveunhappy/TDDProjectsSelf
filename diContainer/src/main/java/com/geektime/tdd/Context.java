8c5ee4f3511db7e3e399a055d842f987482d7973
houjiangtao <925147954@qq.com>
2025-09-06 23:21:18 +0800

    1.增加一个default接口，准备把这两个接口合并，因为返回的是Optional，所以去掉.get() 2.方法是从InjectionProvider的    private static Object toDependency(Context context, Type type) {                                 if (type instanceof ParameterizedType) return context.get((ParameterizedType) type).get();                                 return context.get((Class<?>) type).get();                             }拷贝过来的

--- diContainer/src/main/java/com/geektime/tdd/Context.java
+++ diContainer/src/main/java/com/geektime/tdd/Context.java
@@ -1,9 +1,15 @@
 package com.geektime.tdd;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Optional;
 
 public interface Context {
     <Type> Optional<Type> get(Class<Type> type);
     Optional<Object> get(ParameterizedType type);
+
+    default Optional getType(Type type) {
+        if (type instanceof ParameterizedType) return get((ParameterizedType) type);
+        return get((Class<?>) type);
+    }
 }
