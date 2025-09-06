49771e1c2f7b1e327754d7d8c80c6084e8128765
houjiangtao <925147954@qq.com>
2025-09-07 00:28:26 +0800

    1.优化为一个if-else

--- diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
+++ diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
@@ -27,16 +27,18 @@ public class ContextConfig {
         return new Context() {
             Context context = this;
 
-            private  <Type> Optional<Type> getComponent(Class<Type> type) {
+            private <Type> Optional<Type> getComponent(Class<Type> type) {
                 //在Java中，当在内部类或匿名类中使用 this 时，它指的是该内部类或匿名类的实例，而不是外部类的实例，
                 // 所以这里的 this 就是指代当前创建的 Context 匿名实现类的实例本身
                 return Optional.ofNullable(providers.get(type)).map(provider -> (Type) provider.get(this));
             }
+
             @Override
             public Optional get(Type type) {
                 if (isContainer(type)) return getContainer((ParameterizedType) type);
                 return getComponent((Class<?>) type);
             }
+
             private Optional<Object> getContainer(ParameterizedType type) {
                 if (type.getRawType() != Provider.class) return Optional.empty();
 
@@ -55,17 +57,14 @@ public class ContextConfig {
 
     private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
         for (Type dependency : providers.get(component).getDependencies()) {
-            if (dependency instanceof Class) {
-                checkComponentDependency(component, visiting, (Class<?>) dependency);
-            }
-            if (isContainer(dependency)) {
-                checkContainerTypeDependency(component, dependency);
-            }
+            if (isContainer(dependency)) checkContainerTypeDependency(component, dependency);
+            else checkComponentDependency(component, visiting, (Class<?>) dependency);
         }
     }
 
     private void checkContainerTypeDependency(Class<?> component, Type dependency) {
-        if (!providers.containsKey(getComponentType(dependency))) throw new DependencyNotFoundException(component, getComponentType(dependency));
+        if (!providers.containsKey(getComponentType(dependency)))
+            throw new DependencyNotFoundException(component, getComponentType(dependency));
     }
 
     private void checkComponentDependency(Class<?> component, Stack<Class<?>> visiting, Class<?> dependency) {
