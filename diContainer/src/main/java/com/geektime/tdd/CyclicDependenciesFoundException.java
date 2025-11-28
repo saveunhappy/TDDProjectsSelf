dc9db1f7c3756ea4cddbf36aa73206e197669372
houjiangtao <925147954@qq.com>
2025-11-28 18:05:54 +0800

    1.getComponents先不改，之前的测试还在用

--- diContainer/src/main/java/com/geektime/tdd/CyclicDependenciesFoundException.java
+++ diContainer/src/main/java/com/geektime/tdd/CyclicDependenciesFoundException.java
@@ -5,18 +5,12 @@ import java.util.List;
 import java.util.Set;
 
 public class CyclicDependenciesFoundException extends RuntimeException{
-    private Set<Class<?>> components = new HashSet<>();
-    public CyclicDependenciesFoundException(Class<?> component) {
-        components.add(component);
-    }
-    public CyclicDependenciesFoundException(Class<?> component, CyclicDependenciesFoundException e) {
-        components.add(component);
-        components.addAll(e.components);
-    }
-    public CyclicDependenciesFoundException(List<Class<?>> visiting) {
+    private Set<Component> components = new HashSet<>();
+
+    public CyclicDependenciesFoundException(List<Component> visiting) {
         components.addAll(visiting);
     }
     public Class<?>[] getComponents() {
-        return components.toArray(Class<?>[]::new);
+        return components.stream().map(c->c.type()).toArray(Class[]::new);
     }
 }
