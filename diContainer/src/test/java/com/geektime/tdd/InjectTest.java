98ed091b2265a18f4c8627285c7ea8a30c34bea3
houjiangtao <925147954@qq.com>
2025-12-01 17:36:40 +0800

    1.修改测试的名称

--- diContainer/src/test/java/com/geektime/tdd/InjectTest.java
+++ diContainer/src/test/java/com/geektime/tdd/InjectTest.java
@@ -156,7 +156,7 @@ public class InjectTest {
         @Nested
         public class WithQualifier {
             @Test
-            public void should_include_qualifier_with_dependency() {
+            public void should_include_dependency_with_qualifier() {
                 InjectionProvider<InjectConstructor> provider = new InjectionProvider<>(InjectConstructor.class);
                 assertArrayEquals(new ComponentRef<?>[]{
                                 ComponentRef.of(Dependency.class, new NamedLiteral("chosenOne"))
