a623cd0bb21cd8773fb127016fec68eb2f712ff4
houjiangtao <925147954@qq.com>
2025-12-11 10:10:02 +0800

    1.这个SingletonProvider现在也应是一个单独的类了，抽取出来

--- diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
+++ diContainer/src/main/java/com/geektime/tdd/ContextConfig.java
@@ -75,27 +75,6 @@ public class ContextConfig {
                                                      Function<ComponentProvider<?>, ComponentProvider<?>> provider) {
         scopes.put(scope, provider);
     }
-    static class SingletonProvider<T> implements ComponentProvider<T> {
-        private T singleton;
-        private ComponentProvider<T> provider;
-
-        public SingletonProvider(ComponentProvider<T> provider) {
-            this.provider = provider;
-        }
-
-        @Override
-        public T get(Context context) {
-            if (singleton == null) {
-                singleton = provider.get(context);
-            }
-            return singleton;
-        }
-
-        @Override
-        public List<ComponentRef<?>> getDependencies() {
-            return provider.getDependencies();
-        }
-    }
 
     public Context getContext() {
         components.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
